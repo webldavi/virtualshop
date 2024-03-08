11a154716a3c7495cd3a416094136f1d40e544d9
Luis Davi <luisdavi.contato@gmail.com>
2024-03-08 09:41:24 -0300

    create manager product service

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,3 +1,11 @@
+interface ProductOptions {
+  name: string;
+  imageUrl: string;
+  description: string;
+  price: number;
+  stock: number;
+}
+
 export const managerUser = {
   async auth(token: string) {
     return handleAuthorization(token);
@@ -8,6 +16,41 @@ export const managerUser = {
       body: {
         email: credentials.email,
         password: credentials.password,
+      },
+    });
+  },
+};
+
+export const managerProduct = {
+  async getAllProducts() {
+    return $fetch("/api/products");
+  },
+  async getUniqueProduct(id: string) {
+    return $fetch("/api/products/" + id);
+  },
+  async createProduct(token: string, productOptions: ProductOptions) {
+    return $fetch("/api/products/create", {
+      method: "POST",
+      body: {
+        authorization: token,
+        product: productOptions,
+      },
+    });
+  },
+  async deleteUniqueProduct(token: string, id: string){
+    return $fetch('/api/products/delete/' + id,{
+      method: "DELETE",
+      body: {
+        authorization: token
+      }
+    })
+  },
+  async updateProduct(token: string, productOptions: ProductOptions, id: string) {
+    return $fetch("/api/products/update/" + id, {
+      method: "PUT",
+      body: {
+        authorization: token,
+        product: productOptions,
       },
     });
   },
